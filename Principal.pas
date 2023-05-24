77dc4106595bf18ccd6136de4546dc723d92da9b
Francisco Sáez <fjsaez@gmail.com>
2023-05-24 00:10:54 -0400

    Creación menú principal 1

--- Principal.pas
+++ Principal.pas
@@ -4,10 +4,21 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.ListView.Types,
+  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, FMX.ListView,
+  FMX.MultiView, FMX.ListBox;
 
 type
   TForm1 = class(TForm)
+    LayPrincipal: TLayout;
+    ToolBar1: TToolBar;
+    SBMenu: TSpeedButton;
+    MultiView: TMultiView;
+    ListBox1: TListBox;
+    ListBoxItem1: TListBoxItem;
+    ListBoxItem2: TListBoxItem;
+    ListBoxItem3: TListBoxItem;
   private
     { Private declarations }
   public
