10954080af78c1476d2482d11427659e93f5a18a
Francisco Sáez <fjsaez@gmail.com>
2023-06-17 19:44:11 -0400

    Ajuste menor

--- LocCoordenada.dpr
+++ LocCoordenada.dpr
@@ -1,6 +1,11 @@
 ﻿program LocCoordenada;
 
 uses
+  {$IFDEF ANDROID}
+  Androidapi.JNI.App,
+  Androidapi.JNI.GraphicsContentViewText,
+  Androidapi.Helpers,
+  {$ENDIF}
   System.StartUpCopy,
   FMX.Forms,
   Principal in 'Principal.pas' {FPrinc},
@@ -12,6 +17,10 @@ uses
 
 begin
   Application.Initialize;
+  {$IFDEF ANDROID}
+  TAndroidHelper.Activity.getWindow.addFlags(
+	  TJWindowManager_LayoutParams.JavaClass.FLAG_KEEP_SCREEN_ON);
+  {$ENDIF}
   Application.CreateForm(TFPrinc, FPrinc);
   Application.CreateForm(TDMod, DMod);
   Application.Run;
