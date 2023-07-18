776dd365545312c8c1a645ac87f8dd0436304f23
Francisco Sáez <fjsaez@gmail.com>
2023-07-18 00:34:48 -0400

    Agregado módulo Acerca

--- Acerca.pas
+++ Acerca.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Layouts, FMX.Controls.Presentation;
+  FMX.Layouts, FMX.Controls.Presentation, FMX.Objects;
 
 type
   TFrmAcerca = class(TFrame)
@@ -19,6 +19,21 @@ type
     ToolBar1: TToolBar;
     SBVolver: TSpeedButton;
     Label1: TLabel;
+    Image1: TImage;
+    Label2: TLabel;
+    Layout8: TLayout;
+    Label3: TLabel;
+    Label4: TLabel;
+    Layout9: TLayout;
+    Label5: TLabel;
+    Layout10: TLayout;
+    Label6: TLabel;
+    Layout11: TLayout;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    Rectangle1: TRectangle;
     procedure SBVolverClick(Sender: TObject);
   private
     { Private declarations }
